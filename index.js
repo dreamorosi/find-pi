753d689e3706a2976c72d29321da5c3ee27b7dea
Andrea Amorosi <dreamorosi@gmail.com>
2019-06-16 02:13:20 +0200

    :construction: moving to nmap

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const execa = require('execa')
+const findPi = require("./lib/findPi");
 
-// Format result string by extracting IP address between ( )
-const formatResult = string => {
-  let startPos = string.indexOf('(')
-  let endPos = string.indexOf(')')
-  return string.substring(startPos + 1, endPos)
-}
-
-// Execute command and return result
-module.exports = () => execa.shell('arp -na | grep -i B8:27:EB')
-  .then(res => formatResult(res.stdout))
-  .catch(() => 'No Rasperry Pi found on the local network')
+console.log(findPi());
